39b22f56122fa6fa1f47e5df2dc3f8f31a879922
Ali sher <alisher4u23@gmail.com>
2025-08-13 13:05:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let popup = document.getElementById("popup");
+
+// Open popup by adding the class
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+// Close popup by removing the class
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
